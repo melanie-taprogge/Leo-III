bbaeb95f5d3b42782369f219a17d47bad8d31fd1
melanie-taprogge <melanie.taprogge@icloud.com>
2025-06-19 14:50:59 +0200

    Skolemisazion
    and bug fixes

--- src/main/scala/leo/modules/output/LPoutput/CNFEncoding.scala
+++ src/main/scala/leo/modules/output/LPoutput/CNFEncoding.scala
@@ -12,9 +12,23 @@ import leo.modules.output.LPoutput.lpDatastructures._
 
 object CNFEncoding {
 
-  val allBoolRulesTermName = lpConstantTerm("allCNFids_app")
+  val allBoolRulesTermName = lpConstantTerm("boolIds")
 
   val allBoolRuleApplicationStep = lpEval(allBoolRulesTermName)
+
+  val fullCnfTacName = lpConstantTerm("cnfTac")
+  def cnfTac(varsListName: Option[lpConstantTerm],skDefsListName: Option[lpConstantTerm]) = {
+
+    /*
+    val instTac: lpTerm= (varsListName, skDefsListName) match {
+      case (Some(vars), Some(sks)) => lpFunctionApp(fullCnfTacName,Seq(vars,sks))
+      case (Some(vars), None) =>
+      case (None, Some(sks)) =>
+      case (None,None) => allBoolRulesTermName
+    }
+     */
+    lpEval(lpFunctionApp(fullCnfTacName,Seq(varsListName.getOrElse(lpListLast),skDefsListName.getOrElse(lpListLast))))
+}
 
 
 }
--- src/main/scala/leo/modules/output/LPoutput/lpDatastructures.scala
+++ src/main/scala/leo/modules/output/LPoutput/lpDatastructures.scala
@@ -864,7 +864,6 @@ object lpDatastructures {
     val asOlTerm = "#rewrite"
 
     lazy val olTermApp = {
-      assert(!rewritePattern0.isDefined && rwRhs == false, s"LP-Encoding: Trying to use $asOlTerm with rewrite pattern or keyword left")
       rewriteTerm match {
         case t:lpOlTerm =>
           val patternStr = if (rewritePattern0.isDefined) s"\"${rewritePattern0.get}\"" else s"\"\""
@@ -911,6 +910,24 @@ object lpDatastructures {
     override private[lpDatastructures] def openCurlyBracket: String = s"$tabs{${lpAssume(vars).pretty}"
 
     override def toProofScrips: lpProofScript = lpProofScript(Seq(lpAssume(vars, tab)))
+  }
+
+  case class lpSetTac(name: String, dfn: lpStatement, tab: Int = 0) extends lpProofScriptStep(tab: Int) {
+    def addTab(i: Int): lpSetTac = lpSetTac(name, dfn, tab + i)
+
+    val tabs: String = "\t" * tab
+
+    def pretty0: String = {
+      s"set ${name} ≔ ${dfn.pretty}"
+    }
+
+    override def pretty: String = {
+      s"${tabs}${lpSetTac(name, dfn, tab).pretty0}"
+    }
+
+    override private[lpDatastructures] def openCurlyBracket: String = s"${tabs}{${lpSetTac(name, dfn, tab).pretty0}"
+
+    override def toProofScrips: lpProofScript = lpProofScript(Seq(lpSetTac(name, dfn, tab)))
   }
 
   case class lpTacSimplify(tab: Int = 0) extends lpProofScriptStep(tab: Int) {
